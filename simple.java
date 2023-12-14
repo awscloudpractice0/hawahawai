abda0b06b9d5eb991ad917e622b77af7c9e0ed64
dev-1 <developer1@mymail.com>
2023-12-14 19:51:31 +0530

    commit-2

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class Simple{
         public static void main(String args[]){
 
-                System.out.println("Welcome to AWS Cloud Practice India");
+                System.out.println("Welcome to AWS Cloud Practice India-Mumbai");
         }
 }
